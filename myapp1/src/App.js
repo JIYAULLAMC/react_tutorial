c2b670c5cc77e927c7e1efd5ed92ec81dbcaf65f
JIYACHINDADI <jiyachindadi@gmail.com>
2023-08-13 20:57:36 +0530

    composing function base component one into other

--- myapp1/src/App.js
+++ myapp1/src/App.js
@@ -1,16 +1,14 @@
 import React, { Component } from "react";
+import Student from "./Student";
 
-// let ele = <h2>Creating Element in App and exporting to the index file </h2>
-
-class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <h2>Hello World</h2>
-        <h2>React fragment use</h2>
-      </React.Fragment>
-    );
-  }
+const App = () => {
+  return (
+      <div>
+        <Student />
+        <Student />
+        <Student />
+      </div>
+  )
 }
 
 export default App;
---
+++ myapp1/src/Student.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+const Student = () => {
+    return (
+        <h2>This is Student Component</h2>
+    );
+}
+
+export default Student;

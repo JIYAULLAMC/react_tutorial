1255184301dc045752d34fa63bf19a681ed834e8
JIYACHINDADI <jiyachindadi@gmail.com>
2023-08-13 20:20:34 +0530

    react fragment used to return the multiple elements at time

--- myapp1/src/App.js
+++ myapp1/src/App.js
@@ -1,11 +1,16 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 // let ele = <h2>Creating Element in App and exporting to the index file </h2>
 
-class App extends Component{
-    render() {
-        return (React.createElement("h2", null, "This is class base demo react component"));
-    }
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h2>Hello World</h2>
+        <h2>React fragment use</h2>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;

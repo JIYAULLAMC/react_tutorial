cbf42e8d301292eb087a2d4f4324700f944f05d8
JIYACHINDADI <jiyachindadi@gmail.com>
2023-08-19 22:58:12 +0530

    passing the args and event object to event handlers

--- myapp1/src/Student.jsx
+++ myapp1/src/Student.jsx
@@ -2,29 +2,24 @@ import React, {Component} from "react";
 import "./Student.css";
 
 class Student extends Component{  
- 
-  constructor(props){
-    super(props);
-    this.state = {
-      name : "jiyaulla", 
-      roll : this.props.roll
-    }
-  } 
+  
+  // this is the state and props
+  state = {
+    name : "jiyaulla", 
+    roll : this.props.roll
+  }
 
-  handleEventByArrowFunc = () => {
-    console.log("this is arrow function", this )
-    // setState is used to modify the state and props
-    // this.setState({name : "Mohamad Hayat", roll : 125})  // setState take object as argument
-    // this.state.name = "Mohamad Hayat"  // this will change the attribute but react wont recognize it
-    
-    this.setState((state, props) => {  // setState take take function as argument
-      console.log("state are ", state);
-      console.log("props are : ", props);
-      return ({
-      name :"Mahmad Hayat",
-      roll : 100000
-      })
-    })
+  // this is handle the state and props and perform the event
+  handleEventByArrowFunc = (roll, event) => {
+    console.log("button is clicked -----------", roll )  
+    console.log("event -----------", event)  
+  }
+
+  // this will call the event
+  handleClickEventArg = (event) => {
+    return (
+      this.handleEventByArrowFunc(this.state.roll, event)
+    );
   }
   
   render() {
@@ -34,7 +29,18 @@ class Student extends Component{
         <h4>Demonstration of seting the state</h4>
         <h2>Name : {this.state.name} </h2>
         <h2>Roll : {this.state.roll}</h2>
-        <button onClick={this.handleEventByArrowFunc}>Click Me By Arrow Func</button> <br /><br />
+        <button onClick={this.handleClickEventArg}>Click Here</button> <br />
+        <h3> using annonymous function : 
+        <button onClick={ event => this.handleEventByArrowFunc(this.state.roll, event)}>Click Here</button> <br />
+        </h3>
+        <h3> This how to bind the event : 
+        <button onClick={ this.handleClickEventArg.bind(this.state.roll, this)}>Click Here</button> <br />
+        </h3>
+        <br />
+        {/* 
+        <button onClick={this.handleClickEventArg()}>Click Here</button> <br /><br /> 
+        this is not a valid because it will call while rendering 
+        */}
       </div>
   );}
   

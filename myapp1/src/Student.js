ebc9ad3dde50770798baaf41d4fe3a008ea401dd
JIYACHINDADI <jiyachindadi@gmail.com>
2023-08-19 12:45:42 +0530

    passing the props in class base components

--- myapp1/src/Student.js
+++ myapp1/src/Student.js
@@ -1,14 +1,16 @@
-import React from "react";
+import React, {Component} from "react";
 import "./Student.css"
 
-const Student = props => {
-  return (
-    <div>
-      <h4>This is function base component and example for passing the props</h4>
-      <h2>Name : {props.name} </h2>
-      <h2>Age : {props.age} </h2>
-    </div>
-  )
+class Student extends Component{
+  render() {
+    return (
+      <div>
+        <h4>This is function base component and example for passing the props</h4>
+        <h2>Name : {this.props.name} </h2>
+        <h2>Age : {this.props.age} </h2>
+      </div>
+   );
+  }
 }
 
 export default Student;

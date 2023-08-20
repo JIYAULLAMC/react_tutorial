6dfeb137b36a58ac7c20dcef91b4089b2c38198d
JIYACHINDADI <jiyachindadi@gmail.com>
2023-08-20 11:39:25 +0530

    component lifecycle while mounting in DOM

--- myapp1/src/Marks.jsx
+++ myapp1/src/Marks.jsx
@@ -1,14 +1,12 @@
 import React, {Component} from "react";
-import "./Student.css";
-import Marks from "./Marks";
 
-class Student extends Component{  
+class Marks extends Component{  
   
   // lifecycle method Mounting
   // constructor will always call first during mounting
   constructor(props){
     super(props);
-    console.log("component constructor called -------------- student 1")
+    console.log("component constructor called -------------- Marks 1")
     console.log("this is props----",  props)
     this.state = {
       name : "Jiyaulla"
@@ -16,26 +14,25 @@ class Student extends Component{
   }
 
   static getDerivedStateFromProps(props, state){
-    console.log("component getderivedstatefromprops method-------------- student 2")
+    console.log("component getderivedstatefromprops method-------------- Marks 2")
     // you can log here here what you want and you can do whatever you want
     return null;
   }
 
   componentDidMount() {
-    console.log("component did mount ----------------- student 4")
+    console.log("component did mount ----------------- Marks 4")
   }
   
   render() {
-    console.log("component render called ------------------ student 3")
+    console.log("component render called ------------------ Marks 3")
     return (
       <div>
-        <h2>Student Class Base Component</h2>
+        <h2>Marks Class Base Component</h2>
         <h1>During lifecycle just you need to know when and which lifecycle method is calling, what you can do in it</h1>
-        <Marks name = "marks" />
       </div>
   );}
   
 }
 
 
-export default Student;   
+export default Marks;   

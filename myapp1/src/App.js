39c472efd7ed7769105345ebc15a91c03e1e08fa
JIYACHINDADI <jiyachindadi@gmail.com>
2023-09-10 22:25:09 +0530

    adding conditional rendering usesing if statements in render method

--- myapp1/src/App.js
+++ myapp1/src/App.js
@@ -1,24 +1,24 @@
-import React from 'react'
+import React, { Component } from 'react'
 import User from './User';
 import Guest from './Guest';
 
-function App(props) {
 
-  console.log("-------------",props.isUser)
-  if (props.isUser) {
-    return (
+export default class App extends Component {
+  render() {
+    if (this.props.isUser) {
+      return (
+        <div>
+          <h2>User </h2>
+          <User />
+        </div>
+      )
+
+    }
+    return(
       <div>
-        <h3> User  </h3>
-        <User />
-      </div>);
+        <h2>Guest </h2>
+        <Guest />
+      </div>
+    )
   }
-  return (
-    <div>
-      <h3>Guest </h3>
-      <Guest />
-    </div>
-  )
-  
 }
-
-export default App;
--- myapp1/src/index.js
+++ myapp1/src/index.js
@@ -2,4 +2,4 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from "./App"
 
-ReactDOM.render(<App isUser={false}/>, document.getElementById('root'))
+ReactDOM.render(<App isUser={true}/>, document.getElementById('root'))

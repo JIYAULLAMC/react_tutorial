ecc1dbce7e5781db5c4e9746faa96f5b722e5e5e
JIYACHINDADI <jiyachindadi@gmail.com>
2023-09-10 22:02:07 +0530

    using custom hooks

--- myapp1/src/App.js
+++ myapp1/src/App.js
@@ -4,12 +4,15 @@ import useCustomCounter from './Custom';
 
 function App() {
   const data = useCustomCounter();
+  const data1 = useCustomCounter();
   
   
   return (
   <div>
     <h3>Custom Hook Count Up {data.count}</h3>
     <button type="button" onClick={data.handleIncrement}>Increment </button>
+    <h3>Custom Hook Count Up {data1.count}</h3>
+    <button type="button" onClick={data1.handleIncrement}>Increment </button>
   </div>);
 }
 

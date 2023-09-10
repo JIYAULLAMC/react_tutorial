c1a352773def9d4880906b841ce824489a15e423
JIYACHINDADI <jiyachindadi@gmail.com>
2023-09-10 21:57:08 +0530

    how to use custom hooks in react

--- myapp1/src/App.js
+++ myapp1/src/App.js
@@ -1,33 +1,15 @@
 import React, { useState, useEffect } from 'react'
+import useCustomCounter from './Custom';
 
 
 function App() {
-  const [upcount, setUpcount] = useState(0);
-  const [downcount, setDowncount] = useState(1000);
-    
-  const increasCount = () => {
-    console.log("increse+++++++++++++++++++")
-    setUpcount(upcount+10)
-  }
-    
-  const decreasCount = () => {
-    console.log("decrese click here---------------------")
-    setDowncount(downcount-10)
-  }
-
-  useEffect(()=> {
-    console.log("use effect is called here************")
-  }, [upcount])
-  // the second parameter as array will take states 
-  // useEffect will run after only array states will get change 
-  // mean on change upcount useeffect will run for downcount changes it will not run or exicute
-
+  const data = useCustomCounter();
+  
+  
   return (
   <div>
-    <h2>Count Up: {upcount}</h2>
-    <button type='button' onClick={increasCount}>Increase By 10</button>
-    <h2>Count Down: {downcount}</h2>
-    <button type='button' onClick={decreasCount}>Decrease By 10</button>
+    <h3>Custom Hook Count Up {data.count}</h3>
+    <button type="button" onClick={data.handleIncrement}>Increment </button>
   </div>);
 }
 

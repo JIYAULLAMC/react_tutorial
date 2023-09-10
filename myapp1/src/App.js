c2b0c9638b3404e52d9be43a73c51000f2c73251
JIYACHINDADI <jiyachindadi@gmail.com>
2023-09-10 20:40:31 +0530

    using useEffect hook in react

--- myapp1/src/App.js
+++ myapp1/src/App.js
@@ -1,23 +1,33 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 
 function App() {
-  const [name, setName] = useState("Rahul");
-  const [roll, setRoll] = useState(10)
-
+  const [upcount, setUpcount] = useState(0);
+  const [downcount, setDowncount] = useState(1000);
     
-  const handleClick = () => {
-    console.log("handle click here---------------------")
-    setName("Geeky Shows")
-    setRoll(10000000)
+  const increasCount = () => {
+    console.log("increse+++++++++++++++++++")
+    setUpcount(upcount+10)
+  }
+    
+  const decreasCount = () => {
+    console.log("decrese click here---------------------")
+    setDowncount(downcount-10)
   }
 
-
+  useEffect(()=> {
+    console.log("use effect is called here************")
+  }, [upcount])
+  // the second parameter as array will take states 
+  // useEffect will run after only array states will get change 
+  // mean on change upcount useeffect will run for downcount changes it will not run or exicute
 
   return (
   <div>
-    <h2>Name: {name} Roll:{roll}</h2>
-    <button type='button' onClick={handleClick}>Click Here</button>
+    <h2>Count Up: {upcount}</h2>
+    <button type='button' onClick={increasCount}>Increase By 10</button>
+    <h2>Count Down: {downcount}</h2>
+    <button type='button' onClick={decreasCount}>Decrease By 10</button>
   </div>);
 }
 

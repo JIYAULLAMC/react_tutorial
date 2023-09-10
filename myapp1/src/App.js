0918f4a7b048b69a93de03f26d07e2edf8930cf6
JIYACHINDADI <jiyachindadi@gmail.com>
2023-09-10 22:04:36 +0530

    conditional rendering

--- myapp1/src/App.js
+++ myapp1/src/App.js
@@ -1,18 +1,11 @@
-import React, { useState, useEffect } from 'react'
-import useCustomCounter from './Custom';
+import React from 'react'
 
 
 function App() {
-  const data = useCustomCounter();
-  const data1 = useCustomCounter();
-  
   
   return (
   <div>
-    <h3>Custom Hook Count Up {data.count}</h3>
-    <button type="button" onClick={data.handleIncrement}>Increment </button>
-    <h3>Custom Hook Count Up {data1.count}</h3>
-    <button type="button" onClick={data1.handleIncrement}>Increment </button>
+    <h3>Conditional Rendering</h3>
   </div>);
 }
 
